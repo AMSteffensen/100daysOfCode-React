366634d4ce04783970460d7efd23ac7961353fbb
Andreas Steffensen <andreas@Andreass-Air.home>
2021-02-23 17:22:45 +0100

    recommit first commit

---
+++ day1-note-keeper/src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer__text">CopyRight 2021</p>
+        </footer>
+    );
+}
+
+export default Footer;
+
+
---
+++ day1-note-keeper/src/components/Note/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+
+function Note() { 
+    return (
+        <div className="note">
+            <h2 className="note__title">Note</h2>
+            <p className="note__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, animi?</p>
+        </div>
+    )
+}
+
+export default Note;
